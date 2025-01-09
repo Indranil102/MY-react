357d44a6e37d61bb7544c4b6795619cc86b98f0b
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-09 11:35:24 +0530

    basic undersatnding react done

---
+++ firstreactclass/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from "./Chai"
+
+function App() {
+  const username = ' mai to gya '
+
+  return (
+    
+    <>
+    <h1>i am good{username}</h1>
+    <Chai/>
+    </>
+ 
+  )
+}
+
+export default App
---
+++ firstreactclass/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <>
+            <p> i need chaihe</p>
+            </>
+    )
+}
+
+export default Chai
