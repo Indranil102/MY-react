e8bd9211914abd9b03e2cd0d299a7ec10018f100
Indranil102 <indranilsamanta2003@gmail.com>
2025-01-18 17:33:04 +0530

    uploaded

---
+++ NewStart/first_lect/src/App.jsx
@@ -0,0 +1,18 @@
+import Hello from "./Components/Hello"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        hlo world
+        </div>
+
+        <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ NewStart/first_lect/src/Components/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello (){
+    return (
+        <h1>i ma good hello there</h1>
+    )
+}
+
+export default Hello
